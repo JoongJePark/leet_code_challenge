4dfe265ebf5273847f153c02280ecbd38dcac17e
JoongJePark <jjp936@gmail.com>
2021-04-15 22:34:01 +0900

    AddTwoNumbers 답지 참고 및 이해

--- src/AddTwoNumbers.java
+++ src/AddTwoNumbers.java
@@ -1,5 +1,3 @@
-import java.util.List;
-
 public class AddTwoNumbers {
     /**
      * Definition for singly-linked list.
@@ -27,50 +25,56 @@ public class AddTwoNumbers {
             this.next = next;
         }
     }
-
-    ListNode recursive(ListNode l1, ListNode l2, ListNode l3) {
-        if (l1.next.next != null) {
-            if (l1.val + l2.val >= 10) {
-                l3.val += l1.val + l2.val - 10;
-                l3.next = new ListNode(1, new ListNode());
-                recursive(l1.next, l2.next, l3.next);
-            } else {
-                l3.val += l1.val + l2.val;
-                l3.next = new ListNode(0, new ListNode());
-                recursive(l1.next, l2.next, l3.next);
-            }
-        } else if (l1.next.next == null) {
-            if (l1.val + l2.val >= 10) {
-                l3.val += l1.val + l2.val - 10;
-                l3.next = new ListNode(1);
-                recursive(l1.next, l2.next, l3.next);
-            } else {
-                l3.val += l1.val + l2.val;
-                recursive(l1.next, l2.next, l3.next);
-            }
-        } else if (l1.next == null) {
-            if (l1.val + l2.val >= 10) {
-                l3.val += l1.val + l2.val - 10;
-                l3.next = new ListNode(1);
-            } else {
-                l3.val += l1.val + l2.val;
-            }
-        }
-        return l3;
-    }
-
+// 내 풀이(틀림)
+//    ListNode recursive(ListNode l1, ListNode l2, ListNode l3) {
+//        if (l1 == null && l2 == null) {
+//            return null;
+//        } else if (l1.next == null && l2.next == null) {
+//            if (l1.val + l2.val >= 10) {
+//                l3.val += l1.val + l2.val - 10;
+//                l3.next = new ListNode(1);
+//            } else {
+//                l3.val += l1.val + l2.val;
+//            }
+//        } else if (l1.next.next == null && l2.next.next == null) {
+//            if (l3.val + l1.val + l2.val >= 10) {
+//                l3.val += l1.val + l2.val - 10;
+//                l3.next = new ListNode(1);
+//                recursive(l1.next, l2.next, l3.next);
+//            } else {
+//                l3.val += l1.val + l2.val;
+//                recursive(l1.next, l2.next, l3.next);
+//            }
+//        } else {
+//            if (l3.val + l1.val + l2.val >= 10) {
+//                l3.val += l1.val + l2.val - 10;
+//                l3.next = new ListNode(1, new ListNode());
+//                recursive(l1.next, l2.next, l3.next);
+//            } else {
+//                l3.val += l1.val + l2.val;
+//                l3.next = new ListNode(0, new ListNode());
+//                recursive(l1.next, l2.next, l3.next);
+//            }
+//        }
+//        return l3;
+//    }
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if (l1 == null) {
-            return null;
-        } else if (l1.next == null) {
-            ListNode l3 = new ListNode();
-            if (l1.val + l2.val >= 10) {
-                l3.val = l1.val + l2.val - 10;
-                l3.next = new ListNode(1);
-            } else {
-                l3.val = l1.val + l2.val;
-            }
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
         }
-        return recursive(l1, l2, new ListNode());
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
     }
 }
