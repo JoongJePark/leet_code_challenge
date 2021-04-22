cb95f634d63300ce64ee7ffdebd35299c5c8ce97
JoongJePark <jjp936@gmail.com>
2021-04-22 21:57:49 +0900

    ValidParentheses 해결. discuss에 따르면 map 이용해서 코드 길이 줄일 수 있음

--- src/ValidParentheses.java
+++ src/ValidParentheses.java
@@ -1,5 +1,35 @@
+import java.util.Stack;
+
 public class ValidParentheses {
     public boolean isValid(String s) {
-
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.add(s.charAt(i));
+            } else if (s.charAt(i) == '{') {
+                stack.add(s.charAt(i));
+            } else if (s.charAt(i) == '[') {
+                stack.add(s.charAt(i));
+            } else if (s.charAt(i) == ')') {
+                if (!stack.isEmpty() && stack.peek() == '(') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else if (s.charAt(i) == '}') {
+                if (!stack.isEmpty() && stack.peek() == '{') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else if (s.charAt(i) == ']') {
+                if (!stack.isEmpty() && stack.peek() == '[') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
     }
 }
