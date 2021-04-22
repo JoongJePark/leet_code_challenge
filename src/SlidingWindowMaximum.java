7c867ab5b59247ea59ce03c70498d40e6893d06a
joongje_park <joongje_park@tmax.co.kr>
2021-04-22 19:30:46 +0900

    SlidingWindowMaximum 해결

--- src/SlidingWindowMaximum.java
+++ src/SlidingWindowMaximum.java
@@ -1,71 +1,35 @@
 import java.util.Collections;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class SlidingWindowMaximum {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length;
-        Queue<Integer> queue = new LinkedList<>();
         int[] sol = new int[n - k + 1];
-        int localBest;
-        int localWorst;
+        Deque<Integer> deque = new LinkedList<>();
         for (int i = 0; i < k; i++) {
-            queue.offer(nums[i]);
-        }
-        localBest = Collections.max(queue);
-        localWorst = Collections.min(queue);
-        sol[0] = localBest;
-        for (int i = k; i < n; i++) {
-            if (localBest == queue.peek() && localWorst == queue.peek()) {
-                queue.remove();
-                localBest = (queue.isEmpty()) ? Integer.MIN_VALUE : Collections.max(queue);
-                localWorst = (queue.isEmpty()) ? Integer.MAX_VALUE : Collections.min(queue);
-                if (localBest <= nums[i]) {
-                    localBest = nums[i];
-                }
-                if (localWorst >= nums[i]) {
-                    localWorst = nums[i];
-                }
-                if (localWorst <= nums[i]) {
-                    queue.offer(nums[i]);
-                }
-            } else if (localBest == queue.peek()) {
-                queue.remove();
-                localBest = (queue.isEmpty()) ? Integer.MIN_VALUE : Collections.max(queue);
-                if (localBest <= nums[i]) {
-                    localBest = nums[i];
-                }
-                if (localWorst >= nums[i]) {
-                    localWorst = nums[i];
-                }
-                if (localWorst <= nums[i]) {
-                    queue.offer(nums[i]);
-                }
-            } else if (localWorst == queue.peek()) {
-                queue.remove();
-                localWorst = (queue.isEmpty()) ? Integer.MAX_VALUE : Collections.min(queue);
-                if (localBest <= nums[i]) {
-                    localBest = nums[i];
-                }
-                if (localWorst >= nums[i]) {
-                    localWorst = nums[i];
-                }
-                if (localWorst <= nums[i]) {
-                    queue.offer(nums[i]);
-                }
+            if (deque.isEmpty()) {
+                deque.addLast(i);
             } else {
-                queue.remove();
-                if (localBest <= nums[i]) {
-                    localBest = nums[i];
+                System.out.println(deque.peekLast());
+                while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
+                    deque.removeLast();
                 }
-                if (localWorst >= nums[i]) {
-                    localWorst = nums[i];
-                }
-                if (localWorst <= nums[i]) {
-                    queue.offer(nums[i]);
-                }
+                deque.addLast(i);
             }
-            sol[i - k + 1] = localBest;
+        }
+        sol[0] = nums[deque.peekFirst()];
+        for (int i = k; i < n; i++) {
+            while (!deque.isEmpty() && deque.peekFirst() < i - k) {
+                deque.removeFirst();
+            }
+            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
+                deque.removeLast();
+            }
+            deque.addLast(i);
+
+            sol[i - k + 1] = nums[deque.peekFirst()];
         }
         return sol;
     }
