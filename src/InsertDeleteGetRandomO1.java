a92eafa2db867f7bca0d0fcc3b4310be4c04819e
joongje_park <joongje_park@tmax.co.kr>
2021-04-23 17:52:52 +0900

    InsertDeleteGetRandomO1 해결. 빠른 풀이는 마지막 값과 지운 값만 바꿔줌

--- src/InsertDeleteGetRandomO1.java
+++ src/InsertDeleteGetRandomO1.java
@@ -1,24 +1,55 @@
-public class InsertDeleteGetRandomO1 {
-    class RandomizedSet {
+import java.lang.reflect.Array;
+import java.util.*;
 
-        /** Initialize your data structure here. */
-        public RandomizedSet() {
+class RandomizedSet {
+    /**
+     * Initialize your data structure here.
+     */
+    Map<Integer,Integer> hm;
+    ArrayList<Integer> al;
+    Random random;
 
+    public RandomizedSet() {
+        hm = new HashMap<>();
+        al = new ArrayList<Integer>();
+        random = new Random();
+    }
+
+    /**
+     * Inserts a value to the set. Returns true if the set did not already contain the specified element.
+     */
+    public boolean insert(int val) {
+        if (hm.containsKey(val)){
+            return false;
+        } else {
+            hm.put(val, al.size());
+            al.add(al.size(), val);
+            return true;
         }
+    }
 
-        /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
-        public boolean insert(int val) {
-
+    /**
+     * Removes a value from the set. Returns true if the set contained the specified element.
+     */
+    public boolean remove(int val) {
+        if (hm.containsKey(val)){
+            int value = hm.get(val);
+            hm.remove(val);
+            for (int i = value+1; i<al.size(); i++){
+                hm.put(al.get(i),i-1);
+            }
+            al.remove(value);
+            hm.remove(val);
+            return true;
+        } else {
+            return false;
         }
+    }
 
-        /** Removes a value from the set. Returns true if the set contained the specified element. */
-        public boolean remove(int val) {
-
-        }
-
-        /** Get a random element from the set. */
-        public int getRandom() {
-
-        }
+    /**
+     * Get a random element from the set.
+     */
+    public int getRandom() {
+        return al.get(random.nextInt(al.size()));
     }
 }
