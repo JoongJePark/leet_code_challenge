591bd73cd8091bb1928de7d17f344229f9728720
joongje_park <joongje_park@tmax.co.kr>
2021-05-12 16:44:56 +0900

    GroupAnagrams 내풀이 time limit. char Array sort이용해서 풀이 참조

--- src/GroupAnagrams.java
+++ src/GroupAnagrams.java
@@ -3,41 +3,60 @@ import java.util.*;
 public class GroupAnagrams {
     public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> sol = new ArrayList<>();
-        HashMap<String, HashSet<Character>> map = new HashMap<>();
-        HashMap<String, List<String>> map2 = new HashMap<>();
-        boolean overlap = true;
-        if(strs.length < 1){
-            return sol;
+        if (strs.length == 0) {
+            return new ArrayList();
         }
-        HashSet<Character> firstInput = new HashSet<>();
-        for(int i=0; i<strs[0].length(); i++){
-            firstInput.add(strs[0].charAt(i));
-        }
-        map.put(strs[0], firstInput);
-        map2.put(strs[0], new ArrayList<>(Arrays.asList(strs[0])));
-        HashSet<Character> input = new HashSet<>();
-        for (int i = 1; i < strs.length; i++) {
-            for (String key : map.keySet()) {
-                for (int j = 0; j < strs[i].length(); j++) {
-                    overlap &= map.get(key).contains(strs[i].charAt(j));
-                    input.add(strs[i].charAt(j));
-                }
-                if (overlap && strs[i].length() == key.length()) {
-                    map2.get(key).add(strs[i]);
-                } else {
-                    map2.put(strs[i], new ArrayList<>(Arrays.asList(strs[i])));
-                    map.put(strs[i], input);
-                    input.clear();
-                    overlap = true;
-                    break;
-                }
-                input.clear();
-                overlap = true;
+        HashMap<String, List> map = new HashMap<String, List>();
+        for (String str : strs) {
+            char[] charArr = str.toCharArray();
+            Arrays.sort(charArr);
+            String keyString = String.valueOf(charArr);
+            if (map.containsKey(keyString)) {
+                map.get(keyString).add(str);
+            } else {
+                map.put(keyString, new ArrayList<>());
+                map.get(keyString).add(str);
             }
         }
-        for (String key : map2.keySet()) {
-            sol.add(map2.get(key));
+        for (String key : map.keySet()) {
+            sol.add(map.get(key));
         }
         return sol;
+//        HashMap<String, HashMap<Character, Integer>> map = new HashMap<>();
+//        HashMap<String, List<String>> map2 = new HashMap<>();
+//        boolean overlap = true;
+//        if (strs.length < 1) {
+//            return sol;
+//        }
+//        Loop1:
+//        for (int i = 0; i < strs.length; i++) {
+//            HashMap<Character, Integer> input = new HashMap<>();
+//            for (int j = 0; j < strs[i].length(); j++) {
+//                if (input.containsKey(strs[i].charAt(j))) {
+//                    int nOfChar = input.get(strs[i].charAt(j)) + 1;
+//                    input.put(strs[i].charAt(j), nOfChar);
+//                } else {
+//                    input.put(strs[i].charAt(j), 1);
+//                }
+//            }
+//            Loop2:
+//            for (String key : map.keySet()) {
+//                for (Character key2 : map.get(key).keySet()) {
+//                    if (!input.containsKey(key2) || !(input.get(key2).equals(map.get(key).get(key2)))) {
+//                        continue Loop2;
+//                    }
+//                }
+//                if (strs[i].length() == key.length()) {
+//                    map2.get(key).add(strs[i]);
+//                    continue Loop1;
+//                }
+//            }
+//            map2.put(strs[i], new ArrayList<>(Arrays.asList(strs[i])));
+//            map.put(strs[i], input);
+//        }
+//        for (String key : map2.keySet()) {
+//            sol.add(map2.get(key));
+//        }
+//        return sol;
     }
 }
