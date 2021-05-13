9f03ca2581a5e91f5c12cc2095bf6232b385c1f2
joongje_park <joongje_park@tmax.co.kr>
2021-05-13 15:52:59 +0900

    LongestCommonPrefix 그냥 푸는거 완료

--- src/LongestCommonPrefix.java
+++ src/LongestCommonPrefix.java
@@ -1,16 +1,24 @@
+import java.util.Arrays;
+
 public class LongestCommonPrefix {
     public String longestCommonPrefix(String[] strs) {
-        if(strs.length == 0){
+        if (strs.length == 0) {
             return new String();
         }
-        String sol = strs[0];
-        for (int i=1; i<strs.length; i++){
-            for(int j=0; j< Math.min(sol.length(), strs[i].length()); j++){
-                if(sol.charAt(j)==strs[i].charAt(j)){
-
+        Arrays.sort(strs);
+        int pointer = strs[0].length();
+        for (int i = 1; i < strs.length; i++) {
+            if (pointer == 0) {
+                return new String();
+            }
+            for (int j = 0; j < Math.min(pointer, strs[i].length()); j++) {
+                if (!(strs[0].charAt(j) == strs[i].charAt(j))) {
+                    pointer = j;
+                    break;
                 }
+
             }
         }
-        return sol;
+        return strs[0].substring(0, pointer);
     }
 }
