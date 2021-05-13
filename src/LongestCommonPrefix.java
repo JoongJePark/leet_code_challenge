e5b0602c833fbb6c87857d21971fc0aacf61ce95
joongje_park <joongje_park@tmax.co.kr>
2021-05-13 17:23:45 +0900

    LongestCommonPrefix faster than 100퍼니까 기분좋네

--- src/LongestCommonPrefix.java
+++ src/LongestCommonPrefix.java
@@ -5,13 +5,13 @@ public class LongestCommonPrefix {
         if (strs.length == 0) {
             return new String();
         }
-        Arrays.sort(strs);
         int pointer = strs[0].length();
         for (int i = 1; i < strs.length; i++) {
             if (pointer == 0) {
                 return new String();
             }
-            for (int j = 0; j < Math.min(pointer, strs[i].length()); j++) {
+            pointer = Math.min(pointer, strs[i].length());
+            for (int j = 0; j < pointer; j++) {
                 if (!(strs[0].charAt(j) == strs[i].charAt(j))) {
                     pointer = j;
                     break;
