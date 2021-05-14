e1edf5032840951011ebc54a79c9cffc033336b6
joongje_park <joongje_park@tmax.co.kr>
2021-05-14 14:01:46 +0900

    RegularExpressionMatching solution 보고 해결

--- src/RegularExpressionMatching.java
+++ src/RegularExpressionMatching.java
@@ -1,14 +1,31 @@
 public class RegularExpressionMatching {
     public boolean isMatch(String text, String pattern) {
-        if (pattern.isEmpty()) return text.isEmpty();
-        boolean first_match = (!text.isEmpty() &&
-                (pattern.charAt(0) == text.charAt(0) || pattern.charAt(0) == '.'));
-
-        if (pattern.length() >= 2 && pattern.charAt(1) == '*') {
-            return (isMatch(text, pattern.substring(2)) ||
-                    (first_match && isMatch(text.substring(1), pattern)));
-        } else {
-            return first_match && isMatch(text.substring(1), pattern.substring(1));
+        if (pattern.isEmpty()) {
+            return text.isEmpty();
         }
+        if (text.isEmpty()) {
+            return pattern.length() >= 2 && pattern.charAt(1) == '*' && isMatch(text, pattern.substring(2));
+        }
+        boolean checkFirst = (pattern.charAt(0) == text.charAt(0) || pattern.charAt(0) == '.');
+        if (pattern.length() >= 2 && pattern.charAt(1) == '*') {
+            return isMatch(text, pattern.substring(2)) || (checkFirst && isMatch(text.substring(1), pattern));
+        }
+        return checkFirst && isMatch(text.substring(1), pattern.substring(1));
     }
 }
+
+//leetcode solution
+//class Solution {
+//    public boolean isMatch(String text, String pattern) {
+//        if (pattern.isEmpty()) return text.isEmpty();
+//        boolean first_match = (!text.isEmpty() &&
+//                (pattern.charAt(0) == text.charAt(0) || pattern.charAt(0) == '.'));
+//
+//        if (pattern.length() >= 2 && pattern.charAt(1) == '*'){
+//            return (isMatch(text, pattern.substring(2)) ||
+//                    (first_match && isMatch(text.substring(1), pattern)));
+//        } else {
+//            return first_match && isMatch(text.substring(1), pattern.substring(1));
+//        }
+//    }
+//}
