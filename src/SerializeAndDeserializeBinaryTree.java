d069c18adfb9f543beca57346bb12909b038c638
joongje_park <joongje_park@tmax.co.kr>
2021-05-17 20:00:17 +0900

    Serialize and Deserialize Binary Tree 문제 이해

---
+++ src/SerializeAndDeserializeBinaryTree.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Codec {
+    //Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    // Encodes a tree to a single string.
+//    public void serialRecur(TreeNode root, Queue queue) {
+//        while (!queue.isEmpty()) {
+//            queue.add(root.left);
+//            queue.add(root.right);
+//            depth +=1;
+//        }
+//    }
+    public String serialize(TreeNode root) {
+
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+
+    }
+}
